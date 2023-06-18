c5b67591e8af8db0942daa59d01a36ff6b3674cb
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-18 16:00:55 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main -A program that print and that piece of art is useful" - Dora Korpar, 2015-10-19 and return 1
- *Return: 1
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
