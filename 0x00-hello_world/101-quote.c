e68c6ac56fde858b4ffb8960e379ab84d74a75cf
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-18 15:43:20 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -A program that print and that piece of art is useful" - Dora Korpar, 2015-10-19 and return 1
+ *Return: 1
+ */
+int main(void)
+{
+	write(2,
+	       "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	       59);
+	return (1);
+}
