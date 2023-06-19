141a27c43b7c32ab06da74a7538c1b65f9f8ca6d
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-19 11:51:31 -0700

    lowercase then uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,22 +5,18 @@
  */
 int main(void)
 {
-        char c = 'a';
-
-        while (c <= 'z')
-        {
-                putchar(c);
-                c++;
-        }
-
-        c = 'A';
-
-        while (c <= 'Z')
-        {
-                putchar(c);
-                c++;
-        }
-
-        putchar('\n');
-        return (0);
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
