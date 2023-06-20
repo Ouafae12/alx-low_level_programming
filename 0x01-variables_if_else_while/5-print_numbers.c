771a78a982cb3242a4b255eaf57e8133e5d4b662
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-19 17:20:13 -0700

    nimbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,19 +2,17 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * main - A program that prints all single digit numbers of base 10 starting from 0
+ * main - prints all single digit numbers of base 10 starting from 0
  * Return: 0
  */
 int main(void)
 {
-	char c = '0';
-
-	while (c <= '9')
-	{
-		putchar(c);
-		c++;
-	}
-
-	putchar('\n');
-	return (0);
+char c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
