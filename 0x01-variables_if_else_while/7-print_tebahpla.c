f344fae4b8efc72de42ca07b826413f1ef700e2c
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-19 18:01:33 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,11 +3,10 @@
 #include <time.h>
 /**
  * main - prints the lowercase alphabet in reverse
- * return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 char rev;
 for (rev = 'z'; rev >= 'a'; rev--)
 putchar(rev);
