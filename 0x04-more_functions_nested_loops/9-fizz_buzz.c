414176690d1bb158b2c2068d1d6a3f7a69c7d171
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-22 21:08:44 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - print a fizz-buzz program
+ * main - print a Fizz-Buzz program
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-	if ((x % 3 == 0) && (x % 5 == 0))
+	if ((x % 5 == 0) && (x % 3 == 0))
 	{
 	printf("FizzBuzz");
 	}
 	else if (x % 5 == 0)
 	{
-	printf("buzz");
+	printf("Buzz");
 	}
 	else if (x % 3 == 0)
 	{
-	printf("fuzz")
+	printf("Fizz");
 	}
 	else
 	{
@@ -31,6 +31,6 @@ int main(void)
 	printf(" ");
 	}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
