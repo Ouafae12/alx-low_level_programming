b4cc8006bd24db2141c79748b73f91ef78a9edb6
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-27 19:26:58 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _strlen - a function that returns the length of a string
+ * _strlen - a function that returns the length of a string
  * @s: The input
  * Return: The length of the string
  */
@@ -9,5 +9,6 @@ int _strlen(char *s)
 	int index;
 
 	for (index = 0; s[index] != '\0' ; index++)
+		;
 	return (index);
 }
