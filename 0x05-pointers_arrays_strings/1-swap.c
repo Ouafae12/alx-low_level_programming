f09cf9905e53a44fcf1eab108bf574079a87180a
Ouafae12 <ouafae.charbi1@gmail.com>
2023-06-27 19:15:40 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * swap_in - a function that swaps the values of two integers
+ * swap_int - a function that swaps the values of two integers
  * @a: The first entry
  * @b: The second entry
  */
