1f80b642b3608fe5b45d367c6057033ff1943c67
Ouafae12 <ouafae.charbi1@gmail.com>
2023-07-21 18:33:22 -0700

    array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 #include <stddef.h>
 
+int main(void);
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
